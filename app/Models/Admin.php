525d26824a7e81d2c48a786bbc7b78dc7ce08afc
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-12-28 15:44:16 +0000

    make Admin migration, modal and authentication init

---
+++ app/Models/Admin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'full_name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'bool',
+        'is_admin' => 'bool',
+    ];
+}
