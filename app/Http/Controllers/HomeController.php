c571cda4fdccdecabf6dacc700c893065773e12d
Abdourahmane Diop <cabraldiop18@gmail.com>
2021-01-02 14:03:37 +0000

    Middle : Personal manager

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,16 +7,6 @@ use Illuminate\Contracts\Support\Renderable;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth')->except('index');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return Renderable
@@ -25,14 +15,5 @@ class HomeController extends Controller
     {
         return view('welcome');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return Renderable
-     */
-    public function home()
-    {
-        return view('home');
-    }
+    
 }
