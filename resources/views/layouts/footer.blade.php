c2d3fc508a78f6bd0134c422cef448488af7b2ce
Abdourahmane Diop <cabraldiop18@gmail.com>
2021-01-15 20:00:20 +0000

    feat: add version on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        Faite par  <b><a href="https://empro-sn.com">EMPRO</a></b>
+        Faite par  <b><a href="https://empro-sn.com">EMPRO</a> - Version bêta 1.0.0</b>
     </div>
     <div> &copy; Copyright Ecole Biscuiterie 2020-{{ Carbon\carbon::now()->year }} .</div>
 </footer>
